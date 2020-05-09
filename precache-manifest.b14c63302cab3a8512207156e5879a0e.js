a1fc2be1b8582760d505ce7e9e423191e0da4a7b
Charlotte Mountain <charr.mt@gmail.com>
2020-05-09 10:31:13 -0700

    Updates

---
+++ precache-manifest.b14c63302cab3a8512207156e5879a0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47fe72de9f1af9af543592c600d89719",
+    "url": "/directory/index.html"
+  },
+  {
+    "revision": "4feef3e323d025ae9295",
+    "url": "/directory/static/css/main.18077a78.chunk.css"
+  },
+  {
+    "revision": "e2a14f053243f5ad7e8f",
+    "url": "/directory/static/js/2.78234798.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/directory/static/js/2.78234798.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4feef3e323d025ae9295",
+    "url": "/directory/static/js/main.937f3853.chunk.js"
+  },
+  {
+    "revision": "e6fcde4b5224bf4437f2",
+    "url": "/directory/static/js/runtime-main.373e7b1b.js"
+  }
+]);
